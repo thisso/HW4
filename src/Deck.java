a7fb421b2db06f1eb90d940ab9651cb8f2a5e1c8
David Lee <david.lee.jr@outlook.com>
2024-10-30 06:34:52 -0400

    changes to shuffle for loops for card deck creation

--- src/Deck.java
+++ src/Deck.java
@@ -3,17 +3,17 @@ import java.util.Collections;
 import java.util.List;
 
 public class Deck {
-    private List<Card> cards;
-    private int currentCard;
+    private List<Card> cards; //list to store a collection of cards
+    private int currentCard; //use to track current position
 
     public Deck() {
         cards = new ArrayList<>();
         String[] suits = {"H", "D", "C", "S"};
         String[] ranks = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
 
-        for (String suit : suits) {
-            for (String rank : ranks) {
-                cards.add(new Card(rank, suit));
+        for (int i = 0; i < suits.length; i++) {
+            for (int j = 0; j < ranks.length; j++) {
+                cards.add(new Card(ranks[j], suits[i]));
             }
         }
         shuffle();
